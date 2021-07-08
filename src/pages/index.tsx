6a5249834e6a76e6cd9ebfb10e67e28e119de70b
Francyelle <francyellem64@gmai.com>
2021-07-08 10:44:42 -0300

    project base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1> Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Bom dia!</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  );
 }
