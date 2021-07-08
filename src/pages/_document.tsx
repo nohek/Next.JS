acf3f731d7df24b36d66fdb28585af1eb169fa29
Francyelle <francyellem64@gmai.com>
2021-07-08 14:36:38 -0300

    components and pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;900&display=swap" rel="stylesheet" />
 
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
           <title>ig.news</title>
         </Head>
 
