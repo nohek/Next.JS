6a3d9f51cdd3dccf626d170537cb2650a611179d
Francyelle <francyellem64@gmai.com>
2021-07-09 16:47:43 -0300

    stripe error

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButton {
@@ -5,8 +8,30 @@ interface SubscribeButton {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButton) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    //se tiver logado => criaçao da checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout(sessionId);
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
